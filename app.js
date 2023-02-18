d0f68cab44d148a251af933898ece97806b24f66
EricHerrera02 <86853683+EricHerrera02@users.noreply.github.com>
2023-02-18 19:03:54 -0300

    Servir sitio web
    Se aplican unos template para levantar un sitio web generico

--- app.js
+++ app.js
@@ -2,18 +2,22 @@ const express = require('express')
 const app = express()
 const port = 8080
 
-app.get('/', (req, res) => {
-  res.send('Hello World')
-});
+// Servir contenido estatico
+app.use( express.static('public') );
 
-app.get('/hola-mundo', (req, res) => {
-    res.send('Hola Mundo nueva ruta')
-  })
-  
+app.get('/generic', (req, res) => {
+  res.sendFile(__dirname + '/public/generic.html')
+})
+
+app.get('/elements', (req, res) => {
+  res.sendFile(__dirname + '/public/elements.html')
+})
+
 app.get('*', (req, res) => {
-    res.send('404 | page no fund')
+    res.sendFile(__dirname + '/public/404.html')
   })
 
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
+    console.log(`Example app listening at  http://localhost:${port}`)
   })
+ 
